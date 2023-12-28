f84cf812cd7045ec0cf36e5407ca128620cc59ff
shakil.ahmed <shakil.ahmed@amiprobashi.com>
2023-12-28 16:15:03 +0600

    1st commit

--- Test_Cases/test_practice_automation.py
+++ Test_Cases/test_practice_automation.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from Page_Objets.practice_test_automation import LogIn, NavigateMenu, SubmitContact
 
 
-class TesCase__01:
-    valid_user_name = "studen"
+class TestLogIn:
+    valid_user_name = "student"
     invalid_user_name = "Student"
 
     valid_password = "Password123"
@@ -12,6 +12,18 @@ class TesCase__01:
 
     url = "https://practicetestautomation.com/practice-test-login/"
 
-    def __init__(self,driver):
-        self.driver = driver
-
+    def test_valid_data(self, setup):
+        self.driver = setup
+        self.lp = LogIn(self.driver)
+        self.lp.set_username("student")
+        self.lp.set_password("Password")
+        self.lp.click_login_button()
+        self.nm = NavigateMenu(self.driver)
+        self.nm.click_home_link()
+        self.nm.click_practice_link()
+        self.nm.click_courses_link()
+        self.nm.click_blog_link()
+        self.nm.click_contact_link()
+        self.sc = SubmitContact(self.driver)
+        self.sc.set_first_name("Test")
+        self.sc.set_last_name("12")
